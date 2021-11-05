2ea3c6b5b246c219ddf3bcb5b867578ec8967c86
patelshani7112 <patelshani7112@gmail.com>
2021-11-05 06:14:42 -0400

    filter is successfully added

--- my-app/src/components/Expenses/Expanses.js
+++ my-app/src/components/Expenses/Expanses.js
@@ -7,10 +7,26 @@ import "./Expanses.css";
 function Expanses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
+  const filterExpanses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  let expenseContent = <p>No Expenses Found</p>;
+
+  if (filterExpanses.length > 0) {
+    expenseContent = filterExpanses.map((expense) => (
+      <ExpanseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -19,14 +35,7 @@ function Expanses(props) {
           onChangeFilter={filterChangeHandler}
         />
 
-        {props.items.map((expense) => (
-          <ExpanseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
